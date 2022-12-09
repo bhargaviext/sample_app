ee0f7994e653e7db8bed4f4bf5233d6d6a20548e
bhargaviext <bhargavi.neppalli@extwebtech.in>
2022-12-09 18:11:40 +0530

    Finalise

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+	def full_title(page_title = '')
+		base_tiile = "Ruby on Rails Tutorial Sample App"
+		if page_title.empty?
+			base_tiile
+		else
+			page_title + "|" + base_tiile 
+		end
+	end
 end
